2a9c565f34eaf6090a24202c7ba1879601032afd
Jonny Eduardo Banach <jonnybanach@gmail.com>
2021-08-19 09:53:17 -0300

    Projeto Exemplo Ionic
    Projeto desenvolvido durante a disciplina de Programação III na Universidade do Estado de Santa Catarina - UDESC

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,45 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  num = 0;
+
+  constructor(public alertController: AlertController) {}
+
+  somar(){
+    this.num += 1;
+  }
+
+  subtrair(){
+    this.num -= 1;
+  }
+
+  zerar(){
+    this.exibirAlerta();
+  }
+
+  async exibirAlerta() {
+    const alert = await this.alertController.create({
+      header: 'Atenção',
+      message: 'Você deseja zerar o contador?',
+      buttons: [
+        {
+          text: 'Não',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('O contador não foi zerado.');
+          }
+        }, {
+          text: 'Sim',
+          handler: () => {
+            this.num = 0;
+            console.log('O contador foi zerado.');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
 }
